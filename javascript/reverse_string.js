0a5f446a090d1f179af0d3c6cbe75f1e40ad5fc0
Rachel Hamby <tnthreat33@gmail.com>
2022-12-20 19:19:45 -0500

    commit tree fc8172b8a03b81a81d711dd174e5fd3fef07e5c1

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,9 +1,18 @@
 function reverseString(str) {
-  // type your code here
+  let letterArray = str.split('');
+  let reverseArray = [];
+  for(let i=0; i< letterArray.length; i++){
+      reverseArray.push(letterArray[letterArray.length-1-i]);
+  }
+  let reverseWord = reverseArray.join('');
+  return reverseWord;
 }
 
 if (require.main === module) {
   // add your own tests in here
+console.log("Expecting: 'rachel")
+console.log("=>" , reverseString("lehcar"))
+
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
 
@@ -15,5 +24,16 @@ if (require.main === module) {
 
 module.exports = reverseString;
 
-// Please add your pseudocode to this file
+// rewrite question
+  //take a string(word) reverse the word so when the function is run it gives the arguement in reverse
+
+  //* Please add your pseudocode to this file
+//assign a new array to the hold the letters of the reverse word
+//split the word into individual characters
+//add those characters to the new array
+//reverse the array
+//join the array back together to create the new word
+//the output is the new word 
+
+
 // And a written explanation of your solution
